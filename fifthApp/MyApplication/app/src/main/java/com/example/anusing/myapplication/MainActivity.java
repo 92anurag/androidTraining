7d2e14396e2a9664af29ece8876925dcc0b54457
Anurag SInghal <anusing@adobe.com>
2016-06-07 17:39:28 +0530

    ListVIew

---
+++ fifthApp/MyApplication/app/src/main/java/com/example/anusing/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.anusing.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityListView;
+    String[] cities;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityListView = (ListView) findViewById(R.id.listView);
+        // getResources gives acces of the resources
+        cities = getResources().getStringArray(R.array.citiies);
+        // to populate list views we need adapters
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,cities);
+
+        //attach the adapter to a list view
+        cityListView.setAdapter(adapter);
+
+    }
+}
