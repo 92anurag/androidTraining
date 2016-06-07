c09c8b425ac36d6bf302526f9b65443093e47ce9
Anurag SInghal <anusing@adobe.com>
2016-06-07 11:22:19 +0530

    COuntry App created

---
+++ third App/MyApplication/app/src/main/java/com/example/anusing/countryinfoapp/AddCounrtyActivity.java
@@ -0,0 +1,41 @@
+package com.example.anusing.countryinfoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCounrtyActivity extends AppCompatActivity {
+
+    EditText countryNameEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_counrty);
+        countryNameEditText = (EditText) findViewById(R.id.editText);
+    }
+    // intent has 2 purposes to pass data and launch an activity
+    public void done(View view){
+        String countryName = countryNameEditText.getText().toString();
+        // create an empty intent
+        // this is done to pass data around
+        // is this was a new intent will be launched , we dont want that , we want to use the prevoius instance of countryListActivity inten
+        // Intent intent = new Intent(this,CountryListActivity.class);
+        Intent intent = new Intent();
+        intent.putExtra("COUNTRY_NAME",countryName);
+
+        // send intent to the parent activity
+        // intent are independent
+        // so we need to setup a relation , where to pass this intent
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+    public void cancel(View view){
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+
+}
---
+++ third App/MyApplication/app/src/main/java/com/example/anusing/countryinfoapp/CountryListActivity.java
@@ -0,0 +1,66 @@
+package com.example.anusing.countryinfoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+
+import java.util.ArrayList;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    TextView countryListTextView;
+    ArrayList<String> countryList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+        countryListTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    private String getStringFromArrayList(ArrayList<String> list){
+        StringBuffer buffer = new StringBuffer();
+        String text =  null;
+        for(String name : list){
+            buffer.append(name);
+            buffer.append("\n");
+        }
+        return buffer.toString();
+    }
+
+    public void addNewCountry(View view){
+        Intent intent = new Intent(this,AddCounrtyActivity.class);
+        // 101 is identifier for the child
+        // each child activity should have a unique code
+        startActivityForResult(intent,101);
+    }
+
+    // when of AddCountry List is called intent is passed to CountryList Added
+    // capturing that
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 101 && resultCode == RESULT_OK){
+            String countryName = data.getStringExtra("COUNTRY_NAME");
+            Log.i("CountryListActivity","Received "+ countryName + " from AddCountryList");
+            // this will append to the list
+            // countryListTextView.append("\n" + countryName);
+            countryList.add(countryName);
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String text = getStringFromArrayList(countryList);
+        countryListTextView.setText(text);
+    }
+
+    // activity goes into different modes 1. Active  2. Pause  3. Stop
+    // when another activity start pervious is stopped .
+    // now this 2nd activity finishes 1st activity gets active and we have onStart , onResume methods
+    // Pause happens when we have a dialog box
+}
