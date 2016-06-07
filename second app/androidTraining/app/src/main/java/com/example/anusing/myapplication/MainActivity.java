565495a40dee7d36f75ef3635b2d8c9a59c5f4b5
Anurag SInghal <anusing@adobe.com>
2016-06-07 10:09:41 +0530

    First Activity And Toast

--- second app/androidTraining/app/src/main/java/com/example/anusing/myapplication/MainActivity.java
+++ second app/androidTraining/app/src/main/java/com/example/anusing/myapplication/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.anusing.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText userEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // This line basically creates a content view of the tree ,creates 4 objects , relative object , textview object, edittext object ,buttonView objet
         setContentView(R.layout.activity_main);
+
+        // get a reference ti the edit text from the content view tree of the activity
+        // this should be after view has inflated i.e after setContentView();
+        userEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void showText(View view){
+        String userText = userEditText.getText().toString();
+        // popup at the bottom which delivers message and disaapears
+        // toast requires activity context and not application context , because in application context there is no window
+        Toast.makeText(this, userText, Toast.LENGTH_SHORT).show();
     }
 }
