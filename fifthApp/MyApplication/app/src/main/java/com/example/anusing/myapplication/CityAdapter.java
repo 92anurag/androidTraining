28ac5ea85b88a09f369e58e160cbd6609e2f5f7b
Anurag SInghal <anusing@adobe.com>
2016-06-07 18:04:14 +0530

    Custom Adapter

---
+++ fifthApp/MyApplication/app/src/main/java/com/example/anusing/myapplication/CityAdapter.java
@@ -0,0 +1,51 @@
+package com.example.anusing.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by anusing on 6/7/16.
+ */
+public class CityAdapter extends BaseAdapter
+{
+
+    String[] cities;
+    Context context;
+
+    public CityAdapter(String[] cities, Context context) {
+        this.cities = cities;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return cities.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return cities[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View mainView = null;
+
+        // inflate layout
+        mainView = LayoutInflater.from(context).inflate(R.layout.row,null);
+        TextView tv = (TextView) mainView.findViewById(R.id.textView);
+
+        String cityName = (String) getItem(position);
+        tv.setText(cityName);
+        return mainView;
+    }
+}
