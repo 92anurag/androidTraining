6f03889009ae0d32ebd8fa8ed36aea59d294336a
Anurag SInghal <anusing@adobe.com>
2016-06-08 10:25:33 +0530

    Load Image from Cache

--- fifthApp/MyApplication/app/src/main/java/com/example/anusing/myapplication/CityAdapter.java
+++ fifthApp/MyApplication/app/src/main/java/com/example/anusing/myapplication/CityAdapter.java
@@ -1,10 +1,14 @@
 package com.example.anusing.myapplication;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -16,14 +20,29 @@ public class CityAdapter extends BaseAdapter
     String[] cities;
     Context context;
 
+    LruCache<Integer, Bitmap> cache ;
+
     // In Android create static handler class
     static class ViewHolder {
         TextView tv;
+        ImageView iv;
     }
 
     public CityAdapter(String[] cities, Context context) {
         this.cities = cities;
         this.context = context;
+
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory()/1024);
+
+        //Use 1.8th of teh available memory cache .
+        final int cacheSize = maxMemory/8;
+
+        cache = new LruCache<Integer, Bitmap>(cacheSize){
+            @Override
+            protected int sizeOf(Integer key, Bitmap value) {
+                return value.getByteCount()/1024;
+            }
+        };
     }
 
     @Override
@@ -51,6 +70,7 @@ public class CityAdapter extends BaseAdapter
             mainView = LayoutInflater.from(context).inflate(R.layout.row, null);
             ViewHolder vh =  new ViewHolder();
             vh.tv = (TextView) mainView.findViewById(R.id.textView);
+            vh.iv = (ImageView) mainView.findViewById(R.id.imageView);
 
             // attach the view holder to the view
             mainView.setTag(vh);
@@ -63,6 +83,16 @@ public class CityAdapter extends BaseAdapter
 
         String cityName = (String) getItem(position);
         viewHolder.tv.setText(cityName);
+
+        Bitmap image = cache.get(R.drawable.tom);
+        if(image != null){
+            viewHolder.iv.setImageBitmap(image);
+        }else{
+            BitmapDrawable drawable = (BitmapDrawable)context.getResources().getDrawable(R.drawable.tom);
+            Bitmap img = drawable.getBitmap();
+            cache.put(R.drawable.tom,img);
+            viewHolder.iv.setImageBitmap(img);
+        }
         return mainView;
     }
 }
